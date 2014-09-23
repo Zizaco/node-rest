d6b81dd8d000030053fe9196bde5ffc0664f0f15
Zizaco <zizaco@gmail.com>
2014-09-22 22:31:47 -0300

    Added more specs to the api behavior

--- test/index.js
+++ test/index.js
@@ -3,7 +3,6 @@ var expect     = require('expect.js')
 
 describe('express rest api server', function(){
   var id
-
   it('post object', function(done){
     superagent.post('http://localhost:3000/collections/test')
       .send({'name': 'John',
@@ -31,11 +30,44 @@ describe('express rest api server', function(){
   it('retrieves a collection', function(done){
     superagent.get('http://localhost:3000/collections/test')
       .end(function(error, res){
-        expect(e).to.eql(null)
+        expect(error).to.eql(null)
         expect(res.body.length).to.be.above(0)
         expect(res.body.map(function (item) {
           return item._id
         })).to.contain(id)
+        done()
+      })
+  })
+
+  it('updated an object', function(done){
+    superagent.put('http://localhost:3000/collections/test/'+id)
+      .send({'name': 'Peter',
+        'email': 'peter@yahoo.com'})
+      .end(function(error, res){
+        expect(error).to.eql(null)
+        expect(typeof res.body).to.eql('object')
+        expect(res.body.msg).to.eql('success')
+        done()
+      })
+  })
+
+  it('checks an updated object', function(done){
+    superagent.get('http://localhost:3000/collections/test/'+id)
+      .end(function(error, res){
+        expect(error).to.eql(null)
+        expect(typeof res.body).to.eql('object')
+        expect(res.body._id.length).to.eql(24)
+        expect(res.body._id).to.eql(id)
+        expect(res.body.name).to.eql('Peter')
+      })
+  })
+
+  it('removes an object', function(done){
+    superagent.del('http://localhost:3000/collections/test/'+id)
+      .end(function(error, res){
+        expect(error).to.eql(null)
+        expect(typeof res.body).to.eql('object')
+        expect(res.body.msg).to.eql('success')
         done()
       })
   })
